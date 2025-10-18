bc021d64f911bed000a1f605897c417c62d75b00
sejin <psj8880@naver.com>
2025-10-18 16:16:56 +0900

    feat(Application): 프로그램 흐름 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,33 @@
 package calculator;
 
+import calculator.View.InputView;
+import calculator.View.OutputView;
+import calculator.domain.Numbers;
+import calculator.dto.RequestDto;
+import calculator.dto.ResponseDto;
+import calculator.service.Adder;
+import calculator.service.Separator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        InputView.guide();
+        String rawInput = Console.readLine();
+        RequestDto requestDto = new RequestDto(rawInput);
+
+        Separator separator = new Separator();
+        separator.extract(requestDto);
+
+        Numbers numbers = new Numbers();
+        numbers.extract(requestDto, separator);
+
+        ResponseDto responseDto = Adder.add(numbers);
+        OutputView.print(responseDto);
+
+
+
     }
+
 }
