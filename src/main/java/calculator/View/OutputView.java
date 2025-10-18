48e226fa5ef3c71a5e6d72e527f4bf4a32674abc
sejin <psj8880@naver.com>
2025-10-18 16:16:14 +0900

    feat(OutputView): 출력 기능 구현

---
+++ src/main/java/calculator/View/OutputView.java
@@ -0,0 +1,11 @@
+package calculator.View;
+
+import calculator.dto.ResponseDto;
+
+public class OutputView {
+
+    public static void print(ResponseDto responseDto) {
+        System.out.print("결과 : ");
+        System.out.println(responseDto.getSum());
+    }
+}
