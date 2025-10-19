014658c622c138b9219b0dbbf14ee2ec7a67bbc8
sejin <psj8880@naver.com>
2025-10-19 17:09:51 +0900

    refactor(Application): 불필요한 주석 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,7 +11,6 @@ import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
         InputView inputView = new ConsoleInputView();
         Separator separator = new DefaultSeparator();
