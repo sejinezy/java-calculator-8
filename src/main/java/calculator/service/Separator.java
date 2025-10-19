b19a7747d1126a6a9238ec7907f47a39baa6e106
sejin <psj8880@naver.com>
2025-10-19 11:51:14 +0900

    feat(Separator): 구분자와 문자열 추출
    - 구분자만 추출했었던 기존 extract()메서드에서 커스텀 구분자 이후 문자열도 추출하도록 기능 추가
    - Numbers에서 했던 문자열 추출을 Separator가 하도록 리팩터링하면서 책임이 명확해짐

--- src/main/java/calculator/service/Separator.java
+++ src/main/java/calculator/service/Separator.java
@@ -4,12 +4,12 @@ import java.util.regex.Pattern;
 
 public class Separator {
 
-    private static final String DEFAULT_SEPARATOR = "[,|:]";
+    private static final String DEFAULT_SEPARATOR = "[,:]";
 
     private Separator() {
     }
 
-    public static String extract(String rawInput) {
+    public static Result extract(String rawInput) {
 
         if (rawInput.startsWith("//")
                 && rawInput.indexOf("\\n") == 3) {
@@ -17,15 +17,23 @@ public class Separator {
             char delim = rawInput.charAt(2);
             validate(delim);
 
-            return Pattern.quote(String.valueOf(delim));
+            String regex = Pattern.quote(String.valueOf(delim));
+
+            int index = rawInput.indexOf("\\n");
+            String numbersPart = rawInput.substring(index + 2);
+
+            return new Result(regex, numbersPart);
         }
-        return DEFAULT_SEPARATOR;
+        return new Result(DEFAULT_SEPARATOR,rawInput);
     }
 
     private static void validate(char delim) {
         if (Character.isDigit(delim)) {
             throw new IllegalArgumentException("커스텀 구분자는 숫자가 될 수 없습니다.");
         }
+    }
+
+    public record Result(String regex, String numbersPart) {
     }
 
 }
