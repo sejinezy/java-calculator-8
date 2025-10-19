3dbe7988bcc77f42464d85b7fa2ab7d20d935a30
sejin <psj8880@naver.com>
2025-10-19 14:15:22 +0900

    refactor(OutputView): 출력 문자열 상수로 관리
    - 변경 용이성을 위해 출력 문자열을 상수로 관리
    - 순수 유틸성 클래스(static 메서드만 존재 , 상태가 없다, 상속할
    이유가 없다) 이므로 final을 클래스 선언부에 붙임

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -2,10 +2,12 @@ package calculator.view;
 
 import calculator.dto.ResponseDto;
 
-public class OutputView {
+public final class OutputView {
+
+    private static final String RESULT_PREFIX = "결과 : ";
 
     public static void print(ResponseDto responseDto) {
-        System.out.print("결과 : ");
+        System.out.print(RESULT_PREFIX);
         System.out.println(responseDto.getSum());
     }
 }
